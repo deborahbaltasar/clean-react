00db5f8f0fe1ed164ed8d172b7a74252b04a2521
Deborah Baltasar <deborahbnogueira.dn@gmail.com>
2021-11-22 19:26:10 -0300

    chore: config sass

--- webpack.config.js
+++ webpack.config.js
@@ -8,12 +8,27 @@ module.exports = {
     path: path.join(__dirname, 'public/js'),
     publicPath: '/public/js',
     filename: 'bundle.js'
-  }, 
+  },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
+    extensions: ['.ts', '.tsx', '.js', 'scss'],
     alias: {
       '@': path.join(__dirname, 'src')
     }
+  },
+  module: {
+    rules: [{
+      test: /\.ts(x?)$/,
+      loader: 'ts-loader',
+      exclude: /node_modules/
+    }, {
+      test: /\.scss$/,
+      use: [
+        { loader: 'style-loader'}, 
+        { loader: "css-modules-typescript-loader"},
+        { loader: 'css-loader', options: { modules: true } },
+        { loader: 'sass-loader'}
+      ]
+    }]
   },
   devServer: {
     contentBase: './public',
